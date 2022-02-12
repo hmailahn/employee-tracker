afa30db81c511c3059be4ac6be7e928bca46d38e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-12 15:51:18 -0600

    placed forgien keys in schema and working

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -11,14 +11,24 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title_name VARCHAR(30) NOT NULL,
     salary DECIMAL (10,2) NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id) 
 );
+
 
 CREATE TABLE employee (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER NOT NULL,
-    manager_id INTEGER -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
+
+
+
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,15 @@
-/*
+INSERT INTO departments (name)
+VALUES
+('Finance'),
+('Legal')
+('IT');
+
+INSERT INTO roles (title_name, salary, department_id)
+VALUES
+('Payroll Supervisor', 60,000.00, 1),
+('Legal Assistant', 70,000.00, 2),
+('Help Desk Associate', 40,000.00, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Janice', 'Smith', )
